f4f010ee79b85ae13af77cdaf445018782b5f6d6
murod4ik <baxabet@gmail.com>
2024-04-02 18:06:52 +0500

    'clear'

--- keyboards/reply.py
+++
@@ -1,9 +0,0 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-def location_btn():
-    
-    builder = ReplyKeyboardBuilder()
-    
-    builder.button(text='📍', request_location=True)
-    
-    return builder
--- state/state.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class SearchLang(StatesGroup):
-    lang = State()
-    
-class WeatherS(StatesGroup):
-    location = State()
